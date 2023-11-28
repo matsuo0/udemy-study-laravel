f56a46230839e48b730b6e34c2e280d0025fbe91
matsuo0 <uno831@gmail.com>
2023-11-28 14:01:09 +0900

    add vite

--- backend/app/Http/Controllers/TestController.php
+++ backend/app/Http/Controllers/TestController.php
@@ -10,9 +10,11 @@ class TestController extends Controller
     public function index()
     {
 
+        
+
         $values = Test::all(); // 全件取得
 
-        // dd($values); // デバッグ用 die + var_dump 処理を止めて内容を確認できる
+        dd($values); // デバッグ用 die + var_dump 処理を止めて内容を確認できる
 
         return view('tests.test', compact('values')); // compact('values') で $values を view に渡す
     }
