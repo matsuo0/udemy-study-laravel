cc449369a6f4c2a4160e47d962e3d4dd9a8c228c
matsuo0 <uno831@gmail.com>
2023-11-30 12:54:03 +0900

    検索 find

--- backend/app/Http/Controllers/ContactFormController.php
+++ backend/app/Http/Controllers/ContactFormController.php
@@ -59,7 +59,10 @@ class ContactFormController extends Controller
      */
     public function show($id)
     {
-        //
+        $contact = ContactForm::find($id);
+
+        return view('contacts.show', compact('contact'));
+
     }
 
     /**
