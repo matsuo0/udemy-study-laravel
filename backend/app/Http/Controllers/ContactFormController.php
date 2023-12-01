5211960e17942fd0b81e0f13b42cc6fc7e510a89
matsuo0 <uno831@gmail.com>
2023-12-01 18:00:32 +0900

    show index

--- backend/app/Http/Controllers/ContactFormController.php
+++ backend/app/Http/Controllers/ContactFormController.php
@@ -61,7 +61,33 @@ class ContactFormController extends Controller
     {
         $contact = ContactForm::find($id);
 
-        return view('contacts.show', compact('contact'));
+        if ($contact->gender === 0){
+            $gender = '男性';
+        } else {
+            $gender = '女性';
+        }
+
+        if ($contact->age === 1){
+            $age = '10代';
+        }
+        if ($contact->age === 2){
+            $age = '20代';
+        }
+        if ($contact->age === 3){
+            $age = '30代';
+        }
+        if ($contact->age === 4){
+            $age = '40代';
+        }
+        if ($contact->age === 5){
+            $age = '50代';
+        }
+        if ($contact->age === 6){
+            $age = '60代';
+        }
+
+
+        return view('contacts.show', compact('contact', 'gender', 'age'));
 
     }
 
